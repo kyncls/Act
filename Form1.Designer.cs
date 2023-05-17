237c22fca644a8b739a5cf9179f170a49dc8015b
Noxx <85883822+kyncls@users.noreply.github.com>
2023-05-17 09:07:22 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace LibSys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtauthor = new System.Windows.Forms.TextBox();
+            this.txttitle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtno = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.grid1 = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(260, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Library System";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnEdit);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtauthor);
+            this.groupBox1.Controls.Add(this.txttitle);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtno);
+            this.groupBox1.Location = new System.Drawing.Point(12, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(697, 124);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fill Here";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(583, 85);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(583, 53);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(583, 19);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtauthor
+            // 
+            this.txtauthor.Location = new System.Drawing.Point(117, 82);
+            this.txtauthor.Name = "txtauthor";
+            this.txtauthor.Size = new System.Drawing.Size(405, 20);
+            this.txtauthor.TabIndex = 0;
+            // 
+            // txttitle
+            // 
+            this.txttitle.Location = new System.Drawing.Point(117, 56);
+            this.txttitle.Name = "txttitle";
+            this.txttitle.Size = new System.Drawing.Size(405, 20);
+            this.txttitle.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Author";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Accession Number";
+            // 
+            // txtno
+            // 
+            this.txtno.Location = new System.Drawing.Point(117, 30);
+            this.txtno.Name = "txtno";
+            this.txtno.Size = new System.Drawing.Size(405, 20);
+            this.txtno.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grid1);
+            this.groupBox2.Controls.Add(this.txtSearch);
+            this.groupBox2.Location = new System.Drawing.Point(12, 225);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(697, 202);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search / View";
+            // 
+            // grid1
+            // 
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.Location = new System.Drawing.Point(6, 45);
+            this.grid1.Name = "grid1";
+            this.grid1.Size = new System.Drawing.Size(674, 150);
+            this.grid1.TabIndex = 1;
+            this.grid1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid1_CellContentClick);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(6, 19);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(405, 20);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Library System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtauthor;
+        private System.Windows.Forms.TextBox txttitle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtno;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView grid1;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace LibSys
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection con;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Z:\\QQ121\\LibSys.mdb");
+            loadDatagrid();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void loadDatagrid()
+        { 
+        con.Open();
+            OleDbCommand com = new OleDbCommand("Select * from book order by accession_number asc", con);
+            com.ExecuteNonQuery();
+
+            OleDbDataAdapter adap = new OleDbDataAdapter(com);
+            DataTable tab = new DataTable();
+
+            adap.Fill(tab);
+            grid1.DataSource = tab;
+            con.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand com = new OleDbCommand("Insert into book values('" +txtno.Text+ "', '" +txttitle.Text+ "', '"
+                                                + txtauthor.Text + "')",con);
+            com.ExecuteNonQuery();
+
+
+            MessageBox.Show("Succesfully SAVED!","info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            con.Close();
+            loadDatagrid();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string num = txtno.Text;
+
+            DialogResult dr = MessageBox.Show("Are your sure you want to delete this?", "Confirm Deletion",
+                                                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                OleDbCommand com = new OleDbCommand("Delete from book where accession_number= " + num , con);
+                com.ExecuteNonQuery();
+
+                MessageBox.Show("Succesfully DELETED!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("CANCELLED!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);        
+
+            }
+            con.Close();
+            loadDatagrid();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string up = "Update book SET title = '" + txttitle.Text + "', author= '" +
+                         txtauthor.Text + "' where accession_number = " + txtno.Text;
+            OleDbCommand com = new OleDbCommand(up, con);               
+            com.ExecuteNonQuery();
+
+            MessageBox.Show("Successfully UPDATED!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            con.Close();
+            loadDatagrid();
+        }
+
+        private void grid1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtno.Text = grid1.Rows[e.RowIndex].Cells["accession_number"].Value.ToString();
+            txttitle.Text = grid1.Rows[e.RowIndex].Cells["title"].Value.ToString();
+            txtauthor.Text = grid1.Rows[e.RowIndex].Cells["author"].Value.ToString();
+
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            con.Open();
+
+            OleDbCommand com = new OleDbCommand("Select * from book where title like'%"+ txtSearch.Text+"%'", con);
+            com.ExecuteNonQuery();
+            OleDbDataAdapter adap = new OleDbDataAdapter(com);
+            DataTable data = new DataTable();
+
+            adap.Fill(data);
+            grid1.DataSource = data;
+
+            con.Close();
+        }
+    }
+}
